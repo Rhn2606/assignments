ffcc7d81d914e280d4fbc86afb4838f2452973c7
Rohan <redkarrohan2002@gmail.com>
2023-12-19 11:59:16 +0530

    solved anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,13 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
